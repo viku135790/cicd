9c31f3e99a0de24f954b8c2a943f1cb73cdfada2
viku135790 <viku135790@gmail.com>
2023-06-15 23:49:52 +0530

    two_changes

--- TestFile.java
+++ TestFile.java
@@ -1,6 +1,7 @@
 class TestFile{
 	public static void main(String[] args){
 		System.out.println("Test cicd pipe line");
-
+		System.out.println("cicd done");
+		System.out.println("checked sucessfully");
 	}
 }
